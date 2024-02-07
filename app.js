ac1fe62fbf2f738452683c8712e34f5f1ffcb343
su-kun1899 <higedrum.coz@gmail.com>
2024-02-08 01:01:51 +0900

    feat: :wrench: read input csv file

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import {parse} from "csv-parse/sync";
+
 console.log(`Processing started at ${new Date().toISOString()}`)
 
-// TODO ここにコードを書く
+// Read the input file
+const inputFile = fs.readFileSync('input.csv')
+const inputRecords = parse(inputFile, {
+    columns: true,
+    skip_empty_lines: true,
+});
+
+for (const record of inputRecords) {
+    console.log(record);
+}
 
 console.log(`Processing ended at ${new Date().toISOString()}`)
